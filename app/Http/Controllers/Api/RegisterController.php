4b8d7ab3b8c511dfdafc30de7809b31ad9ee1ad9
asraf-mridha <asrafmridha@gmail.com>
2023-03-18 16:51:43 +0600

    Make Validation for register in Api/RegisterController

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -2,10 +2,24 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\BaseController as BaseController;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Validator;
 
-class RegisterController extends Controller
+class RegisterController extends BaseController
 {
-    //
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+
+            'name'      => 'required|string|max:255',
+            'email'     => 'required|email|unique:users',
+            'password'  => 'required|min:6',
+            'confirm_password' => 'required|same:password'
+        ]);
+        if ($validator->fails()) {
+            return $this->sendError('Validation Error', $validator->errors());
+        }
+    }
 }
