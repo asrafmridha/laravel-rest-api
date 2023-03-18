249bf85c2abb8d5ed6c92f4803900f72dca209ec
asraf-mridha <asrafmridha@gmail.com>
2023-03-18 16:37:47 +0600

    Customize Base Controller for responseSuccesss or responseError

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -6,5 +6,27 @@ use Illuminate\Http\Request;
 
 class BaseController extends Controller
 {
-    //
+    public function sendResponse($result, $message)
+    {
+        $response = [
+            'success' => true,
+            'data'    => $result,
+            'message' => $message
+        ];
+
+        return response()->json($response, 200);
+    }
+
+    public function sendError($error, $errorMessage = [], $code = 404)
+    {
+        $response = [
+            'success' => false,
+            'message' => $error
+        ];
+       //$respon array push a new key named data
+        if(!empty($errorMessage)){
+            $response['data']=$errorMessage;
+        }
+        return response()->json($response, $code);
+    }
 }
