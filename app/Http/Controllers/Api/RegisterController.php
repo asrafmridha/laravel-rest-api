825788854fb00530c9037d42f0d4a23fa60d42f2
asraf-mridha <asrafmridha@gmail.com>
2023-03-21 18:56:08 +0600

    Make validation for user login

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -24,12 +24,29 @@ class RegisterController extends BaseController
             return $this->sendError('Validation Error', $validator->errors());
         }
 
-        $password=Hash::make($request->password);
+        $password = Hash::make($request->password);
 
-        $user=User::create([
-            'name'     =>$request->name,
-            'email'    =>$request->email,
+        $user = User::create([
+            'name'     => $request->name,
+            'email'    => $request->email,
             'password' => $password,
         ]);
+        $success['token'] = $user->createToken('RestApi')->plainTextToken;
+        $success['name'] =  $user->name;
+
+        return $this->sendResponse($success, 'User register successfully');
+    }
+
+    public function login(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'email'     => 'required|email|',
+            'password'  => 'required|min:6',
+        ]);
+        
+        if ($validator->fails()) {
+            return $this->sendError("The password or email that you've entered is incorrect", $validator->errors());
+        }
     }
 }
